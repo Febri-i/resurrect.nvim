8188adb12f86973d1e89d9fa17e73ee5f06bb679
Febri-i <febribayun@gmail.comm>
2023-11-11 23:39:01 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+[resurrect with workpaces.nvim](https://i.imgur.com/bTKtA7v.gif)
+
+# Resurrect your neovim session
+
+Yet another session plugin? not really this plugin doesnt use mksession and didnt actually save your "session" its just list all the files that you opened, sort it according to last modified time then save it for you to resurrect them later.
+
+# Why?
+
+mksession sucks. It saves everything, including Nvim-Tree too.
+
+# Setup
+
+Just install it with whatever your plugin manager is, example for lazy.nvim
+
+```
+{
+    'Febri-i/resurrect.nvim'
+}
+
+```
+
+# Config
+
+Using setup function
+
+```lua
+require("resurrect").setup({
+    session_dir = "<put your session directory her, default = ~/.vimsession/>",
+    auto_wipeout = true -- automatically do :bufdo bwipeout before loading session
+})
+```
+Or
+```lua
+vim.g.RessurectSessionDir = "<session dir, default = ~/.Vimsession/>"
+vim.g.RessurectAutoWipeout = true
+```
+
+# Usage
+
+```lua
+require("ressurect").save("session_name_here")
+require("ressurect").load("session_name_here")
+```
---
+++ lua/ressurect.lua
@@ -0,0 +1,102 @@
+vim.g.RessurectSessionDir = "~/.vimsession/"
+vim.g.RessurectSAutoWipeout = true
+
+local function getSessionDir(name)
+	return vim.fn.expand(vim.g.RessurectSessionDir .. "/" .. name .. ".fsession")
+end
+
+local function lstat(path)
+	if not #path then
+		return nil
+	end
+	return vim.loop.fs_lstat(vim.fn.expand(path))
+end
+
+local function realpath(path)
+	if not #path then
+		return nil
+	end
+	return vim.loop.fs_realpath(vim.fn.expand(path))
+end
+
+local function readfile(path)
+	if not #path then
+		return nil
+	end
+	return vim.fn.readfile(vim.fn.expand(path)) or {}
+end
+
+local function writefile(data, path)
+	vim.fn.writefile(data, vim.fn.expand(path))
+end
+
+return {
+	setup = function(param)
+		param = vim.tbl_extend("keep", param, {
+			session_dir = vim.g.RessurectSessionDir,
+			auto_wipeout = vim.g.RessurectSAutoWipeout,
+		})
+		param.session_dir = vim.fn.expand(param.session_dir)
+		vim.g.RessurectSAutoWipeout = param.auto_wipeout
+		if not realpath(param.session_dir) then
+			vim.g.RessurectSessionDir = param.session_dir
+		end
+
+		local session_dir_stat = lstat(vim.g.RessurectSessionDir)
+		if not session_dir_stat then
+			vim.fn.mkdir(vim.fn.expand(vim.g.RessurectSessionDir))
+		end
+	end,
+	save = function(name)
+		vim.print(name)
+		if not (name and #name) then
+			return
+		end
+		local session_path = getSessionDir(name)
+		local bufs = vim.api.nvim_list_bufs()
+		local bufs_stat = vim.tbl_map(function(buf_id)
+			local path = vim.api.nvim_buf_get_name(buf_id)
+			local resultlstat = nil
+			if realpath(path) then
+				resultlstat = lstat(path)
+			end
+			if resultlstat then
+				resultlstat = resultlstat.mtime.nsec
+			end
+			return { name = path, lastmod = resultlstat }
+		end, bufs)
+		local valid_buf_path = vim.tbl_filter(function(buf_stat)
+			return buf_stat.lastmod and buf_stat.lastmod ~= 0
+		end, bufs_stat)
+		table.sort(valid_buf_path, function(a, b)
+			return a.lastmod > b.lastmod
+		end)
+
+		local mapped = vim.tbl_map(function(buf_stat)
+			return buf_stat.name
+		end, valid_buf_path)
+		writefile(mapped, session_path)
+	end,
+	load = function(name)
+		vim.print(name)
+		if not (name and #name) then
+			return
+		end
+		if vim.g.RessurectSAutoWipeout then
+			vim.cmd([[bufdo bwipeout]])
+		end
+		local session_path = getSessionDir(name)
+		if not lstat(session_path) then
+			return
+		end
+		local paths = readfile(session_path)
+		--
+		for _, path in ipairs(paths) do
+			if lstat(path) then
+				vim.schedule(function()
+					vim.cmd.edit(path)
+				end)
+			end
+		end
+	end,
+}
